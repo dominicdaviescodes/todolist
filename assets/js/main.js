515683a8807fbc139fa8a1f12afd8e795ce767b5
dominicdaviescodes <codewithdom@gmail.com>
2020-07-06 12:15:48 +0100

    enable feature click on x to delete a todo

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,12 @@
 $('li').click(function () {
   $(this).toggleClass('completed');
 });
+
+// click on x to delete a todo
+// pass in e event object to stop event bubbling up to parent elements
+$('span').click(function (e) {
+  $(this).parent().fadeOut(500, function(){
+    $(this).remove();
+  })
+  e.stopPropagation();
+});
