867ad9f939aa0b8b07fb7148ca88347804e349cf
dominicdaviescodes <codewithdom@gmail.com>
2020-07-06 15:15:49 +0100

    fix issue where unable to delete elements that werent' on page, when page loads, added event listeners to existing elements

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,12 @@
 // check off specific todos by clicking
-$('li').click(function () {
+// when an li in a ul is clicked run this code
+$('ul').on('click', 'li', function () {
   $(this).toggleClass('completed');
 });
 
 // click on x to delete a todo
 // pass in e event object to stop event bubbling up to parent elements
-$('span').click(function (e) {
+$('ul').on('click', 'span', function (e) {
   $(this)
     .parent()
     .fadeOut(500, function () {
@@ -17,7 +18,7 @@ $('span').click(function (e) {
 $("input[type='text']").keypress(function (e) {
   if (e.which === 13) {
     var todoText = $(this).val();
-    $(this).val("");
-    $("ul").append("<li><span>X</span> " + todoText + "</li>");
+    $(this).val('');
+    $('ul').append('<li><span>X</span> ' + todoText + '</li>');
   }
 });
