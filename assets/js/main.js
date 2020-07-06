41e3a5659e9cbc6eb1579dcc2b4e037aedc1ec39
dominicdaviescodes <codewithdom@gmail.com>
2020-07-06 14:15:31 +0100

    work on using enter key and grabbing value from input

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,8 +6,19 @@ $('li').click(function () {
 // click on x to delete a todo
 // pass in e event object to stop event bubbling up to parent elements
 $('span').click(function (e) {
-  $(this).parent().fadeOut(500, function(){
-    $(this).remove();
-  })
+  $(this)
+    .parent()
+    .fadeOut(500, function () {
+      $(this).remove();
+    });
   e.stopPropagation();
+});
+
+// type a todo and hit enter adds todo item
+
+$("input[type='text']").keypress(function (e) {
+  if (e.which === 13) {
+    var todoText = $(this).val();
+    
+  }
 });
